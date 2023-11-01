69a442f96171d3c3213090bcd96081ed6cadad51
MADS <deejaykbello@hotmail.com>
2023-11-01 15:57:51 -0300

    Fix in git add .Content/Home Components and update the project.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "VINGADORES ANTIGOS DE VOLTA?",
+    "description": "Desde o lançamento de Vingadores: Ultimato e Homem-Aranha: Longe de Casa em 2019, a Marvel Studios tem enfrentado dificuldades para recriar a magia da Saga do Infinito.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2023/09/Capitao-America-Viuva-Negra-e-Homem-de-Ferro.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    "title": "O que isso significa para Kang?",
+    "description": "Parece que a Marvel Studios tem considerado se afastar do personagem, independentemente das consequências do drama judicial.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2023/04/Kang-o-Conquistador-Jonathan-Majors.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Noite de Halloween em Nova York.",
+    "description": "O ator foi flagrado levando sua filha para pedir doces ou travessuras durante a noite de Halloween.",
+    "photoCover":"https://th.bing.com/th?id=OIF.s2oQ4cOaX4xHx%2f5mMGF%2bIQ&pid=ImgDet&rs=1"
+  },
+  {
+    "id":"4",
+    "title": "Seria o retorno do Titã Louco?",
+    "description": "O enigmático diálogo do Titã Louco ameaça, “Sempre haverá mais para terminar meu trabalho.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2023/11/Thanos-e-Capita-Marvel.jpg"
+    
+  },
 ]
